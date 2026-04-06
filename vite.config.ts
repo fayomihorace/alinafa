4092569df741e9a11dad96c6bcd57cc88a83c549
fayh-j <fayomihorace@gmail.com>
2026-04-06 18:47:44 +0100

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/alinafa/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
